1270f4d46b679022d3b295506b17ed2a786aca3f
David Kinkaku <davidkinkaku1@gmail.com>
2021-08-13 18:45:08 -0500

    updates

---
+++ this.js
@@ -0,0 +1,15 @@
+$().ready(function() {
+
+    // click event handler
+    // adding row on click 
+    // removing row on click
+    console.log('The DOM is ready');
+
+
+
+
+
+});
+
+
+// Declaring an array for the objects of
